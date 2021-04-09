c50cfbcd195e2c9834e35bb2f0654e2d37657de7
bgdnvk <bnovykov@gmail.com>
2021-04-09 05:27:18 +0200

    prime

--- current/Prime number decompositions.js
+++ current/Prime number decompositions.js
@@ -4,7 +4,21 @@
 const assert = require('chai').assert
 
 function getAllPrimeFactors(n) { 
-    return ""
+    const arr = []
+
 }
 //Test.assertSimilar(getAllPrimeFactors(100), [2,2,5,5]);
-assert.equal(getAllPrimeFactors(100), [2,2,5,5]);
+//assert.equal(getAllPrimeFactors(100), [2,2,5,5]);
+
+// https://stackoverflow.com/questions/39429564/to-find-a-number-is-prime-why-checking-till-n-2-is-better-what-is-the-reason-f
+const isPrime = (n) => {
+    for(let i = 2; i <= n/2; i++){
+        console.log(i);
+       if(n % i === 0){
+          return false;
+       }
+    };
+    return true;
+ };
+
+console.log(isPrime(25)); 
