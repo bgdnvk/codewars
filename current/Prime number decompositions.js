49100d92cd0e45d84e4aed950812028e36110913
bgdnvk <bnovykov@gmail.com>
2021-04-09 05:17:08 +0200

    chai

---
+++ current/Prime number decompositions.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/53c93982689f84e321000d62/train/javascript
+//https://stackoverflow.com/questions/61817528/vscode-no-debug-adapter-can-not-send-variables
+
+const assert = require('chai').assert
+
+function getAllPrimeFactors(n) { 
+    return ""
+}
+//Test.assertSimilar(getAllPrimeFactors(100), [2,2,5,5]);
+assert.equal(getAllPrimeFactors(100), [2,2,5,5]);
