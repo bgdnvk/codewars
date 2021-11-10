b055a9eb946a28eb587bb3ed3364c7d66ce02609
bgdnvk <bnovykov@gmail.com>
2021-11-10 06:53:47 +0100

    .

---
+++ leetcode/squares_sorter_arr.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/squares-of-a-sorted-array/
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var sortedSquares = function(nums) {
+    let arr = [];
+    for (let i of nums){
+        arr.push(i*i)
+    }
+    console.log(arr)
+    arr.sort((a,b) => a-b)
+    console.log(arr)
+    return arr
+};
