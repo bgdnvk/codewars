c2de38a350508e0d3904b74e65f690434a373897
bgdnvk <bnovykov@gmail.com>
2021-11-16 00:50:30 +0100

    fucking test cases

--- leetcode/duplicate_zeros.js
+++ leetcode/duplicate_zeros.js
@@ -3,26 +3,41 @@
  * @return {void} Do not return anything, modify arr in-place instead.
  */
  var duplicateZeros = function(arr) {
-     console.log('in funct');
-     console.log(arr);
+    console.log(`input: ${arr}`);
     const indexes = []
     for (let i=0; i<arr.length; i++){
-        console.log(i)
-        if(arr[i] === arr[arr.length-1]){
-            return
-        }
-        if(arr[i] === 0){
+        if(arr[i] === 0 && i !== arr.length-1 && i !== arr.length-2){
             indexes.push(i)
         }
     }
-    console.log(indexes)
-    
-    for(let i of indexes){
-        arr.splice(i,0,"bum")
-    }
-    console.log(arr)
-};
-duplicateZeros([1,0,2,3,0,4,5,0])
 
-//duplicate zeros in an array and push them to the end of the array
-console.log('hihi');
+    console.log(`popd arr: ${arr}`)
+    if(indexes.length !== 0){
+        // arr.splice(indexes[0], 0, 0)
+
+        console.log(`indexes: ${indexes}`)
+        for( let i of indexes){
+            arr.pop()
+        }
+        
+        for (let i=0; i<indexes.length; i++){
+            // arr.splice(indexes[i]+1, 0, 0)
+            // const temp = [...arr]
+            // console.log(`temp: ${temp}`);
+            // temp.splice(indexes[i]+i, 0, 0)
+            // console.log(`temp splice: ${temp}`);
+            console.log(`arr b4 splice: ${arr}`);
+            arr.splice(indexes[i]+i, 0, 0)
+            console.log(`arr after splice: ${arr}`);
+
+        }
+    }
+
+
+    console.log(`result arr ${arr}`)
+    return arr
+};
+// duplicateZeros([1,0,2,3,0,4,5,0])
+// duplicateZeros([1,2,3])
+// duplicateZeros([0,1,7,6,0,2,0,7])
+duplicateZeros([0,1,7,6,0,2,0,0,0,7])
