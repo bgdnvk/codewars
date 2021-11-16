87400419d0943581641ec5ab637ac6596f982f28
bgdnvk <bnovykov@gmail.com>
2021-11-16 01:10:45 +0100

    simple

--- leetcode/duplicate_zeros.js
+++ leetcode/duplicate_zeros.js
@@ -3,40 +3,13 @@
  * @return {void} Do not return anything, modify arr in-place instead.
  */
  var duplicateZeros = function(arr) {
-    console.log(`input: ${arr}`);
-    const indexes = []
-    for (let i=0; i<arr.length; i++){
-        if(arr[i] === 0 && i !== arr.length-1 && i !== arr.length-2){
-            indexes.push(i)
+    for (var i=0; i<arr.length; i++) {
+        if (arr[i] === 0) {
+            arr.splice(i, 0, 0);
+            arr.pop();
+            i+=1
         }
     }
-
-    console.log(`popd arr: ${arr}`)
-    console.log(`indexes: ${indexes}`);
-    if(indexes.length !== 0){
-        // arr.splice(indexes[0], 0, 0)
-
-        console.log(`indexes: ${indexes}`)
-        for( let i of indexes){
-            arr.pop()
-        }
-        
-        for (let i=0; i<indexes.length; i++){
-            // arr.splice(indexes[i]+1, 0, 0)
-            // const temp = [...arr]
-            // console.log(`temp: ${temp}`);
-            // temp.splice(indexes[i]+i, 0, 0)
-            // console.log(`temp splice: ${temp}`);
-            console.log(`arr b4 splice: ${arr}`);
-            arr.splice(indexes[i]+i, 0, 0)
-            console.log(`arr after splice: ${arr}`);
-
-        }
-    }
-
-
-    console.log(`result arr ${arr}`)
-    return arr
 };
 // duplicateZeros([1,0,2,3,0,4,5,0])
 // duplicateZeros([1,2,3])
