c2c95594cb8ab51cdf5252be21abc1bb288a1341
bgdnvk <bnovykov@gmail.com>
2021-11-22 01:48:13 +0100

    bs

--- leetcode/remove_element.js
+++ leetcode/remove_element.js
@@ -3,13 +3,24 @@
  * @param {number} val
  * @return {number}
  */
- var removeElement = function(nums, val) {
+//  var removeElement = function(nums, val) {
     
-    nums = nums.filter(e => e != val)
+//     nums = nums.filter(e => e != val)
 
     
-    console.log(nums.length)
-    console.log(nums)
-    return nums.length
-};
+//     console.log(nums.length)
+//     console.log(nums)
+//     return nums.length
+// };
+
+const removeElement = (nums, val) => {
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === val) {
+        nums.splice(i, 1);
+        i--;
+      }
+    }
+    return nums.length;
+  };
+
 removeElement([3,2,2,3], 3)
