2db47c7ae7fafecabab9f4e56d98816b65346858
bgdnvk <bnovykov@gmail.com>
2021-11-22 18:35:33 +0100

    works but awful performance

--- leetcode/rm dup from sorted arr.js
+++ leetcode/rm dup from sorted arr.js
@@ -8,6 +8,14 @@
     // nums = [...new Set(nums)]
     // return nums.length
     // doesn't work cuz it has to be in place
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === nums[i + 1]) {
+            nums.splice(i, 1)
+            i--
+        }
+    }
+    
 };
 
 removeDuplicates([1,1,2])
+removeDuplicates([0,0,1,1,1,2,2,3,3,4])
