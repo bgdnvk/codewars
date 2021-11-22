a63d6d01709ea567b26807f58175e8617a08ffb8
bgdnvk <bnovykov@gmail.com>
2021-11-22 18:17:03 +0100

    in place err

---
+++ leetcode/rm dup from sorted arr.js
@@ -0,0 +1,13 @@
+//https://leetcode.com/explore/learn/card/fun-with-arrays/526/deleting-items-from-an-array/3248/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var removeDuplicates = function(nums) {
+    // nums = [...new Set(nums)]
+    // return nums.length
+    // doesn't work cuz it has to be in place
+};
+
+removeDuplicates([1,1,2])
