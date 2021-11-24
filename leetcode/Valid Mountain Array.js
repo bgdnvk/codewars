548b8af81d582ab8a43877e2d601c4e9273f7813
bgdnvk <bnovykov@gmail.com>
2021-11-24 21:21:29 +0100

    mountain arr what

---
+++ leetcode/Valid Mountain Array.js
@@ -0,0 +1,8 @@
+//https://leetcode.com/explore/learn/card/fun-with-arrays/527/searching-for-items-in-an-array/3251/
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+ var validMountainArray = function(arr) {
+    
+};
