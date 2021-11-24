be94e322f98ce2ccc28791c12d0e0462c2e47090
bgdnvk <bnovykov@gmail.com>
2021-11-24 21:20:09 +0100

    finished

--- leetcode/Check If N and Its Double Exist.js
+++ leetcode/Check If N and Its Double Exist.js
@@ -5,14 +5,21 @@
  */
  var checkIfExist = function(arr) {
     let res = false;
-    arr.forEach( e => {
+    console.log(`arr is ${arr}`);
+    arr.forEach( (e, i) => {
+        console.log("i is "+i);
         console.log('e is '+e)
-        console.log(arr.indexOf(e*2))
-        if(arr.indexOf(e*2) !== -1 && e*2 !==0){
-            console.log('found')
-            console.log(e*2);
+        console.log("index of res: "+arr.indexOf(e*2));
+        //check if the double of e exists in the array
+        if(arr.indexOf(e*2) !== -1){
+            console.log('inside arr index loop');
+            console.log('e*2 is '+e*2);
             console.log(arr.indexOf(e*2))
-            res = true
+            //check we are not iterating over the same element
+            if(arr.lastIndexOf(e*2) !== arr.indexOf(arr[i])){
+                res = true
+            }
+            console.log('end arr index loop');
             }
         }
     )
@@ -20,10 +27,15 @@
     return res
 };
 
+
+
 //wrong [-2,0,10,-19,4,6,-8]
 //expect false
 // checkIfExist([-2,0,10,-19,4,6,-8])
 
 //wrong [0,0]
 //expect true
-checkIfExist([0,0])
+// checkIfExist([0,0])
+
+// checkIfExist([10,2,5,3])
+//expected true
