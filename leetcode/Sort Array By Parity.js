7a628b64a7b7713179b6299908e0fe5d8d466e23
bgdnvk <bnovykov@gmail.com>
2021-11-27 01:32:37 +0100

    sort arr by parity

---
+++ leetcode/Sort Array By Parity.js
@@ -0,0 +1,8 @@
+//https://leetcode.com/explore/learn/card/fun-with-arrays/511/in-place-operations/3260/
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var sortArrayByParity = function(nums) {
+    
+};
