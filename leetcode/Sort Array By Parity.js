6e4f48be8981b6bad86bb7e4dadd811335e06edb
bgdnvk <bnovykov@gmail.com>
2021-11-27 06:08:15 +0100

    need sleep

--- leetcode/Sort Array By Parity.js
+++ leetcode/Sort Array By Parity.js
@@ -4,5 +4,24 @@
  * @return {number[]}
  */
  var sortArrayByParity = function(nums) {
-    
+     //edge case if arr length is 0
+    if(nums.length === 0) return nums;
+    let oddIndex = 0;
+    let evenIndex = 0;
+    for(let i =0; i< nums.length; i++){
+        if(nums[i] % 2 == 0){
+            console.log(nums[i]);
+            //saving the val that's gonna get rewriten
+            //this is the odd value
+            let temp = nums[evenIndex];
+            console.log("tem is "+temp);
+            oddIndex = i;
+            nums[evenIndex] = nums[i];
+            evenIndex++;
+            nums[oddIndex] = temp;
+            
+        }
+    }
+    console.log(nums);
 };
+sortArrayByParity([3,1,2,4]);
