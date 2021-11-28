f911a6f7a92f3dcb10372ca47c279065ed4a6dc8
bgdnvk <bnovykov@gmail.com>
2021-11-28 05:10:43 +0100

    height check

---
+++ leetcode/Height Checker.js
@@ -0,0 +1,8 @@
+//https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3228/
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+ var heightChecker = function(heights) {
+    
+};
