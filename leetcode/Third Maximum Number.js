881d3f20bb3696a17254c3e09e48dcba9345bc49
bgdnvk <bnovykov@gmail.com>
2021-11-28 08:06:00 +0100

    no more cool one liners doesnt work

--- leetcode/Third Maximum Number.js
+++ leetcode/Third Maximum Number.js
@@ -4,23 +4,15 @@
  * @return {number}
  */
  var thirdMax = function(nums) {
-    let max = 0;
-    let count = 0;
-    
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] > max){
-            max = nums[i];
-            count++;
-        }
-        if(count === 3) {
-            return max;
-        }
-    }
-    console.log(max);
-    return max;
+     //make a copy with spread operator
+     //use Set to remove duplicates
+     //sort the array to identify the 3rd different number
+     //return the result on 3rd position ([2])
+    let thirdNum = [...new Set(nums)].sort((a,b) => b-a)[2];
+    if(thirdNum === undefined) return nums[1];
+    return thirdNum;
 };
 // thirdMax([2,2,3,1]);
 // thirdMax([2,1]);
 // thirdMax([2,2,2,2]);
 thirdMax([2,2,3,1])
-//wrong
