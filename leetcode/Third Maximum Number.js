a8d053ebe7e98d957a05477f5afded676edbbf79
bgdnvk <bnovykov@gmail.com>
2021-11-28 21:40:46 +0100

    fin 3rd max

--- leetcode/Third Maximum Number.js
+++ leetcode/Third Maximum Number.js
@@ -4,15 +4,24 @@
  * @return {number}
  */
  var thirdMax = function(nums) {
-     //make a copy with spread operator
-     //use Set to remove duplicates
-     //sort the array to identify the 3rd different number
-     //return the result on 3rd position ([2])
-    let thirdNum = [...new Set(nums)].sort((a,b) => b-a)[2];
-    if(thirdNum === undefined) return nums[1];
-    return thirdNum;
+    //make a copy of the array without dupes
+    const nonDupArr = [...new Set(nums)];
+    //order the array from largest to smallest
+    nonDupArr.sort((a,b) => b-a);
+    console.log(nonDupArr);
+    //get the third max number after sorting
+    const thirdNum = nonDupArr[2];
+    //if the array is less than 3 return the maximum number
+    //if it's greater than 3 return the third maximum
+    return nonDupArr.length < 3 ? Math.max(...nonDupArr) : thirdNum;
 };
 // thirdMax([2,2,3,1]);
 // thirdMax([2,1]);
 // thirdMax([2,2,2,2]);
-thirdMax([2,2,3,1])
+// thirdMax([2,2,3,1])
+// console.log(thirdMax([2,2,3,1]));
+
+console.log(
+    thirdMax([5,2,2])
+);
+//should be 5
