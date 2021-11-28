a9692a250f776473ec04dadff86ce1259b19c299
bgdnvk <bnovykov@gmail.com>
2021-11-28 05:04:05 +0100

    cleaned n added comments

--- leetcode/Sort Array By Parity.js
+++ leetcode/Sort Array By Parity.js
@@ -6,23 +6,27 @@
  var sortArrayByParity = function(nums) {
      //edge case if arr length is 0
     if(nums.length === 0) return nums;
-    let oddIndex = 0;
+    //keep track where the even index is
     let evenIndex = 0;
     for(let i =0; i< nums.length; i++){
+        //if even
         if(nums[i] % 2 == 0){
             console.log(nums[i]);
             //saving the val that's gonna get rewriten
             //this is the odd value
-            let temp = nums[evenIndex];
-            console.log("tem is "+temp);
-            oddIndex = i;
+            let oddTempVal = nums[evenIndex];
+            console.log("temp is "+oddTempVal);
+            //if the value is even it should go at the beginning
+            //there are no strict rules so any even value will do
             nums[evenIndex] = nums[i];
+            //keep track of the even index
             evenIndex++;
-            nums[oddIndex] = temp;
-            
+            // place the odd value where the even value was
+            nums[i] = oddTempVal;
         }
     }
     console.log(nums);
     return nums;
 };
-sortArrayByParity([3,1,2,4]);
+// sortArrayByParity([3,1,2,4]);
+sortArrayByParity([6,3,1,2,4]);
