16928ad8972b9b38d26311685f58b43a0eb9d4c9
bgdnvk <bnovykov@gmail.com>
2021-11-28 22:12:27 +0100

    finished

--- leetcode/Find All Numbers Disappeared in an Array.js
+++ leetcode/Find All Numbers Disappeared in an Array.js
@@ -4,5 +4,24 @@
  * @return {number[]}
  */
  var findDisappearedNumbers = function(nums) {
-    
+    const max = nums.length;
+    console.log(max);
+    let res = [];
+    for(let i = 1; i <= max; i++){
+        let found = false;
+        
+        for(let j = 0; j < nums.length; j++){
+            if (i === nums[j]){
+                found = true;
+            }
+        }
+        if (!found){
+            res.push(i);
+        }
+    }
+    console.log(res);
+    return res;
 };
+
+findDisappearedNumbers([4,3,2,7,8,2,3,1]);
+// findDisappearedNumbers([1,1]);
