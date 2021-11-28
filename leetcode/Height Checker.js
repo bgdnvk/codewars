6e570ccf1a845de571e80a333b7aec837d7e906e
bgdnvk <bnovykov@gmail.com>
2021-11-28 07:09:57 +0100

    hchecker fin

--- leetcode/Height Checker.js
+++ leetcode/Height Checker.js
@@ -4,5 +4,23 @@
  * @return {number}
  */
  var heightChecker = function(heights) {
-    
+
+    //first copy the array using the spread operator
+    //then sort the array to check the differences
+    const sorted = [...heights].sort((a,b) => a - b)
+
+    console.log(heights);
+    console.log(sorted);
+    //counter for the different elements
+    let diffCount = 0;
+    for(let i = 0; i<heights.length; i++){
+        if(heights[i] !== sorted[i]){
+            console.log("diff");
+            diffCount++;
+        }
+    }
+    console.log(diffCount);
+    return diffCount;
 };
+
+heightChecker([1,1,4,2,1,3]);
