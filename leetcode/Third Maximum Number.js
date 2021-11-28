2ccdacfbc770295ff5394659d30d7a7dc5b2e436
bgdnvk <bnovykov@gmail.com>
2021-11-28 07:25:38 +0100

    nope

--- leetcode/Third Maximum Number.js
+++ leetcode/Third Maximum Number.js
@@ -4,5 +4,23 @@
  * @return {number}
  */
  var thirdMax = function(nums) {
+    let max = 0;
+    let count = 0;
     
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] > max){
+            max = nums[i];
+            count++;
+        }
+        if(count === 3) {
+            return max;
+        }
+    }
+    console.log(max);
+    return max;
 };
+// thirdMax([2,2,3,1]);
+// thirdMax([2,1]);
+// thirdMax([2,2,2,2]);
+thirdMax([2,2,3,1])
+//wrong
