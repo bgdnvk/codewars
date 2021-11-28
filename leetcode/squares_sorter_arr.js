d8d9f9e8a4b859fb7a33f0d8f83368c3a9c2825a
bgdnvk <bnovykov@gmail.com>
2021-11-28 07:14:05 +0100

    comments

--- leetcode/squares_sorter_arr.js
+++ leetcode/squares_sorter_arr.js
@@ -1,4 +1,5 @@
 // https://leetcode.com/problems/squares-of-a-sorted-array/
+//https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3574/
 /**
  * @param {number[]} nums
  * @return {number[]}
