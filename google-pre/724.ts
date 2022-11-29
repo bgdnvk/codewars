7363b195d4fc7f54978533137fc9f39d7395dcc7
bgdnvk <bnovykov@gmail.com>
2022-11-29 23:33:35 +0100

    724 wip

--- google-pre/724.ts
+++ google-pre/724.ts
@@ -3,17 +3,34 @@ function pivotIndex(nums: number[]): number {
     
     let pivot: number = 0;
     let sumI: number = 0;
+    let flag: boolean = false;
     
     for (let i = 0; i < nums.length; i++){
         let sumJ: number = 0;
-        sumI+=nums[i]
-        for(let j = i + 2; j < nums.length; j++){
+        if(i === 0) {
+            sumI = 0
+        } else {
+            sumI+=nums[i]
+            
+        }
+        console.log("i ", i)
+        console.log("sum i ", sumI)
+        for(let j = i + 1; j < nums.length; j++){
+            console.log("-- j", j)
+            console.log("-- j nums:", nums[j])
+            console.log("sum j b4:", j)
             sumJ += nums[j]
+            console.log("sum j ", sumJ)
             if(sumI == sumJ) {
-                pivot = i + 1
+                pivot = i
+                flag = true;
             }
         }
         sumJ = 0;
+    }
+    
+    if (!flag) {
+        return -1
     }
     return pivot
 };
