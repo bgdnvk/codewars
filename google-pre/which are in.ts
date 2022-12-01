2676687c69d21331cc1196c5e9bcea615c6da3fb
bgdnvk <bnovykov@gmail.com>
2022-12-01 22:29:57 +0100

    cw wip

---
+++ google-pre/which are in.ts
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/550554fd08b86f84fe000a58/train/typescript
+export function inArray(a1: string[], a2: string[]): string[] {
+  type WordValue = {
+    word: string;
+    value: number;
+  };
+
+  let w: WordValue = { word: "hi", value: 2 };
+  console.log(w);
+
+  return [""];
+}
