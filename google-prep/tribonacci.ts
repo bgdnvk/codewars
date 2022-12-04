24afd6be16203e2ec4b75be2ce71b7db10542ac6
bgdnvk <bnovykov@gmail.com>
2022-12-04 17:52:18 +0100

    tri n docs

---
+++ google-prep/tribonacci.ts
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/556deca17c58da83c00002db/train/typescript
+export function tribonacci([a, b, c]: [number, number, number], n: number): number[] {
+    // your code here
+    console.log(n)
+    
+    return [0]
+  }
