14505411febb34100467581e43c24b24df4862de
bgdnvk <bnovykov@gmail.com>
2022-12-05 19:44:01 +0100

    tribonacci

--- google-prep/tribonacci.ts
+++ google-prep/tribonacci.ts
@@ -1,7 +1,33 @@
 //https://www.codewars.com/kata/556deca17c58da83c00002db/train/typescript
 export function tribonacci([a, b, c]: [number, number, number], n: number): number[] {
-    // your code here
-    console.log(n)
-    
-    return [0]
+  //edge cases
+  if(n <= 0) {
+    return []
   }
+  if(n === 1) {
+    return [a]
+  }
+  if(n < 3){
+    return [a,b]
+  }
+  
+  let pivot: number = 2;
+  let res: number[] = [a,b,c];
+  
+  function sumPrev(arr: number[], pivot: number): number{
+    let sum: number = 0;
+    
+    for(let i = 0; i < 3; i++){
+      sum += arr[pivot-i];
+    }
+    return sum;
+  }
+  
+  for(let i = 0; i < n-3; i++){
+    let sum: number = sumPrev(res, pivot);
+    pivot++;
+    res.push(sum)
+  }
+  
+  return res;
+}
