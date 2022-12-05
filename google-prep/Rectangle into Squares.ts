a4f16a5459f3ca22b972b293b37842e204fd35ef
bgdnvk <bnovykov@gmail.com>
2022-12-05 23:54:40 +0100

    fun

--- google-prep/Rectangle into Squares.ts
+++ google-prep/Rectangle into Squares.ts
@@ -1,4 +1,33 @@
+//https://www.codewars.com/kata/55466989aeecab5aac00003e/train/typescript
 export const sqInRect=(l:number, w:number): null|number[] =>{
-    // Your code here...
-    return null
+  
+    console.log("new program")
+    console.log("L ", l);
+    console.log("W ", w)
+  
+    if(l === w) {
+      return null
+    }
+    let res: number[] = [];
+    let squares: number = l*w;
+    let totalSquares: number = l*w;
+  
+    while (squares > 0 && totalSquares !== 0){
+      console.log("squares: ", squares);
+      console.log("TOTAL squares: ", totalSquares);
+      
+      let sqrt: number = Math.sqrt(squares);
+      console.log("sqrt: ", sqrt);
+      
+      if(Number.isInteger((sqrt))) {
+        console.log("sqrt FOUND: ", sqrt);
+        res.push(sqrt);
+        totalSquares = totalSquares - sqrt*sqrt;
+        squares=totalSquares;
+      } else { 
+        squares--;
+      }
+    }
+  
+    return res
 }
