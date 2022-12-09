4b7c54ab2cad34ab2793fb0e558c8c0e95c4c23f
bgdnvk <bnovykov@gmail.com>
2022-12-09 23:15:39 +0100

    test pass

--- google-prep/50G/68.ts
+++ google-prep/50G/68.ts
@@ -52,7 +52,93 @@ function fullJustify(words: string[], maxWidth: number): string[] {
         }
     }
 
+    let r: string[] = justifyParagraph(arrText, maxWidth)
+
+    console.log("r is", r)
+
     console.log(arrText)
     
-    return arrText;
+    return r;
 };
+
+function justifyParagraph(arr: string[], mw: number): string[] {
+
+    for(let i = 0; i < arr.length; i++) {
+
+        //justify last
+        if(i === arr.length-1) {
+            //push last
+            let fill: number = mw - arr[i].length
+            console.log("first fill: ", fill)
+            const spaces: string = ' '.repeat(fill)
+            let newArr: string;
+            newArr = [...arr[i], spaces].join("")
+            arr[i] = newArr
+            console.log("newArr: ", newArr)
+            console.log("spaces is:", spaces)
+            console.log("last arr[i]: ", arr[i])
+            console.log("last arr[i] length ", arr[i].length)
+            
+        } else {
+            //justify normal paragraph
+            console.log("not last: ", arr[i])
+            arr[i] = addSpaces(arr[i], mw)
+        }
+    }
+
+    return arr
+
+}
+
+function addSpaces(s: string, mw: number): string {
+
+    //remove last space
+    if(s[s.length-1] === ' '){
+        s = s.slice(0, -1)
+    
+    }
+    let fill: number = mw - s.length
+
+    console.log("s is: ", s)
+    console.log("s length is", s.length)
+    console.log("fill is ", fill)
+
+    let idxArr: number[] = []
+
+    for(let i = 0; i < s.length; i++){
+        console.log("s is: ", s[i])
+        console.log("--i is: ", i)
+        if(s[i] === ' ') {
+            console.log("space found")
+            idxArr.push(i)
+        }
+    }
+
+    //if there are no spaces return
+    //TODO: change
+    //doesn't work with one giant word that might need spaces
+    if(idxArr.length === 0 && fill === 0) {
+        return s
+    }
+
+    let newS: string[] = [...s]
+    if(idxArr.length > 0) {
+        while(fill > 0) {
+            for(let i = 0; i < idxArr.length; i++){
+                if(fill > 0) {
+                    newS[idxArr[i]] = newS[idxArr[i]] + " "
+                    fill-- 
+                }
+            } 
+        }
+    } else {
+        while(fill > 0) {
+            newS.push(" ")
+            fill--
+        }
+    }
+
+    s = newS.join("")
+
+    return s
+}
