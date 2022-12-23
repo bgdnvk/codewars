bf0d7a91f7fb529933ca93112cac025ddf995a4a
bgdnvk <bnovykov@gmail.com>
2022-12-23 20:37:29 +0100

    wip? 31

---
+++ google-prep/dsa structy/031.js
@@ -0,0 +1,64 @@
+class Node {
+    constructor(val) {
+      this.val = val;
+      this.next = null;
+    }
+  }
+  
+  const addLists = (head1, head2) => {
+    console.log('start')
+    let r1 = reverseList(head1)
+    let r2 = reverseList(head2)
+    let sum1 = getNum(r1)
+    let sum2 = getNum(r2)
+    console.log(sum1, sum2)
+    let sum = sum1+sum2
+    let sumList = newList(sum)
+    let res = reverseList(sumList)
+    console.log('res is', res)
+  };
+  
+  const reverseList = (head) => {
+    let curr = head 
+    let prev = null
+    while(curr !== null){
+      
+      const next = curr.next
+      curr.next = prev
+      prev = curr 
+      curr = next 
+    }
+    
+    return prev
+  }
+  
+  const getNum = (head) => {
+    let curr = head
+    let arr = []
+    while(curr !== null){
+      arr.push(curr.val)
+      curr = curr.next
+    }
+    let s = arr.join('')
+    let n = parseInt(s)
+    return n
+  }
+  
+  const newList = (sum) => {
+    console.log(sum)
+    let s = sum.toString()
+    let head = new Node(0)
+    let curr = head
+    for(let i = 0; i < s.length; i++){
+      console.log(curr)
+      curr.val = parseInt(s[i]) 
+      curr.next = curr
+    }
+    console.log('returning head')
+    console.log(head)
+    return head
+  }
+  
+  module.exports = {
+    addLists,
+  };
