a45f033db12eee47ba6086fc2fa73037fd71c50b
bgdnvk <bnovykov@gmail.com>
2022-12-24 04:08:10 +0100

    add reversed lists, sum, reverse

--- google-prep/dsa structy/031.js
+++ google-prep/dsa structy/031.js
@@ -1,70 +1,71 @@
 class Node {
-    constructor(val) {
-      this.val = val;
-      this.next = null;
-    }
+  constructor(val) {
+    this.val = val;
+    this.next = null;
   }
-  
-  const addLists = (head1, head2) => {
-    console.log('start')
-    let r1 = reverseList(head1)
-    let r2 = reverseList(head2)
-    let sum1 = getNum(r1)
-    let sum2 = getNum(r2)
-    console.log(sum1, sum2)
-    let sum = sum1+sum2
-    let sumList = newList(sum)
-    console.log(sumList)
-    let res = reverseList(sumList)
-    console.log('res is', res)
-    return res
-  };
-  
-  const reverseList = (head) => {
-    console.log('reverseList')
-    let curr = head 
-    let prev = null
-    while(curr !== null){
-      
-      const next = curr.next
-      curr.next = prev
-      prev = curr 
-      curr = next 
-    }
+}
+
+const addLists = (head1, head2) => {
+  console.log('start')
+  let r1 = reverseList(head1)
+  let r2 = reverseList(head2)
+  let sum1 = getNum(r1)
+  let sum2 = getNum(r2)
+  console.log(sum1, sum2)
+  let sum = sum1+sum2
+  let sumList = newList(sum)
+  console.log(sumList)
+  //return sumList
+  let res = reverseList(sumList)
+  console.log('res is', res)
+  return res
+};
+
+const reverseList = (head) => {
+  console.log('reverseList')
+  let curr = head 
+  let prev = null
+  while(curr !== null){
     
-    return prev
+    const next = curr.next
+    curr.next = prev
+    prev = curr 
+    curr = next 
   }
   
-  const getNum = (head) => {
-    console.log('getNum')
-    let curr = head
-    let arr = []
-    while(curr !== null){
-      arr.push(curr.val)
-      curr = curr.next
-    }
-    let s = arr.join('')
-    let n = parseInt(s)
-    return n
+  return prev
+}
+
+const getNum = (head) => {
+  console.log('getNum')
+  let curr = head
+  let arr = []
+  while(curr !== null){
+    arr.push(curr.val)
+    curr = curr.next
   }
-  
-  const newList = (sum) => {
-    console.log('newList sum: ',sum)
-    let s = sum.toString()
-    let head = new Node(0)
-    let curr = head
-    for(let i = 0; i < s.length; i++){
-      console.log(curr)
-      let n = new Node(parseInt(s[i]))
-      curr.val = parseInt(s[i]) 
-      curr.next = n
-      curr= curr.next
-    }
-    console.log('returning head')
-    console.log(head)
-    return head
+  let s = arr.join('')
+  let n = parseInt(s)
+  return n
+}
+
+const newList = (sum) => {
+  console.log('newList sum: ',sum)
+  let s = sum.toString()
+  let head = new Node(null)
+  let curr = head
+  for(let i = 0; i < s.length; i++){
+    let n = new Node(parseInt(s[i]))
+    curr.next = n
+    curr= curr.next
+    console.log('curr FOR',curr)
   }
-  
-  module.exports = {
-    addLists,
-  };
+  console.log('curr is',curr)
+  console.log('returning head')
+  console.log(head)
+  return head.next
+}
+
+module.exports = {
+  addLists,
+};
