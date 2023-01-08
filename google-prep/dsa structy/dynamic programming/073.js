f7b688e2ae8300f4be844fbcbd15789096b8ce14
bgdnvk <bnovykov@gmail.com>
2023-01-08 08:27:30 +0100

    73

---
+++ google-prep/dsa structy/dynamic programming/073.js
@@ -0,0 +1,13 @@
+const fib = (n, memo={}) => {
+    if(n in memo) return memo[n]
+    if(n === 0) return 0
+    if(n === 1) return 1
+    
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
+    return memo[n]
+  };
+  
+  module.exports = {
+    fib,
+  };
+  
