4c03eb42f76d10466c335c3dd34a771bcb7bdda0
bgdnvk <bnovykov@gmail.com>
2023-01-12 08:28:40 +0100

    wip

---
+++ leetcode/205 Isomorphic Strings.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+   const sSet = new Set() 
+   const tSet = new Set() 
+   for(let char of s) {
+       sSet.add(char)
+   }
+
+   for(let char of t) {
+       tSet.add(char)
+   }
+
+   return sSet.size === tSet.size
+};
