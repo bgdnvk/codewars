1b88ea2272cc7be8adcf29453b2cffeff1a7399b
bgdnvk <bnovykov@gmail.com>
2023-02-23 21:45:47 +0100

    comments

--- leetcode/14. Longest Common Prefix.ts
+++ leetcode/14. Longest Common Prefix.ts
@@ -1,26 +1,31 @@
 //https://leetcode.com/problems/longest-common-prefix/description/
 function longestCommonPrefix(strs: string[]): string {
-
+    //edge cases
     if(strs.length === 0) return ''
     if(strs.length === 1) return strs[0]
 
     let shortest = shortestWord(strs)
+
     let found = false
     let word = shortest
 
     while(!found) {
-
+        //tracker to find if all the words contain the prefix
         let count = 0
 
         for(let i = 0; i < strs.length; i++) {
+            //compare the first part of the word which has to be the same size as our shortest word
             if(strs[i].slice(0, word.length).includes(word)) {
+                //increase count if the prefixes match
                 count++
             }
         }
 
+        //check if all the words had the prefix
         if(count === strs.length) {
             return word
         } else {
+            //if not then iterate again by slicing the end our shortest word
             word = word.slice(0,-1)
         }
     }
