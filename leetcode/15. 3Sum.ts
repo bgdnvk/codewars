d466a9088fde4f9345ac1291c33a28e49129e5eb
bgdnvk <bnovykov@gmail.com>
2023-02-25 01:23:30 +0100

    fuck

--- leetcode/15. 3Sum.ts
+++ leetcode/15. 3Sum.ts
@@ -3,6 +3,7 @@ function threeSum(nums: number[]): number[][] {
     let map = new Map()
 
     let ordered = nums.sort((a,b) => a-b)
+    console.log(ordered)
 
     for(let i = 0; i < ordered.length; i++) {
 
@@ -14,13 +15,33 @@ function threeSum(nums: number[]): number[][] {
 
     let twoMap = new Map()
 
-    console.log(ordered)
-
-    for(let i = 0; i < ordered.length; i++) {
-
-        let arr = map.get(nums[i])
-        
+    for(const [k, v] of map) {
+        console.log(`k: ${k}, v: ${v}`)
+        for(let i = 0; i < v.length; i++) {
+            let curr = getUniqueArr(v[i], v)
+            console.log('curr', curr)
+            twoMap.set([k, v[i]], curr)
+        }
     }
+
+    console.log(twoMap)
+
+    const res = []
+
+    for(const [k, v] of twoMap) {
+        console.log('k', k)
+        console.log('v', v)
+        const [k1, k2] = k
+        let sum = k1+k2
+        console.log('sum', sum)
+        for(let ele of v) {
+            if(sum+ele === 0) {
+                res.push([k1,k2,ele])
+            }
+        }
+    }
+
+    console.log(res)
 
 
     return []
@@ -32,7 +53,7 @@ function getUniqueArr(num: number, nums: number[]): number[] {
     let res: number[] = []
 
     for(let i = 0; i < nums.length; i++) {
-        if(nums[i] !== num) res.push(nums[i])
+        if(nums[i] !== num && !res.includes(nums[i])) res.push(nums[i])
     }
 
     return res
