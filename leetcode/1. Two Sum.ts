b200541c73173e8d5c723c514b20f78f5378e80d
bgdnvk <bnovykov@gmail.com>
2023-05-08 23:33:45 +0200

    1

---
+++ leetcode/1. Two Sum.ts
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/two-sum/description/
+function twoSum(nums: number[], target: number): number[] {
+    const res: number[] = []
+
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = i+1; j < nums.length; j++) {
+            if(nums[i] + nums[j] === target) res.push(i, j)
+        }
+    }
+
+    return res
+};
