bf3f17bf1835cd37a1336126746e90741c66c23f
bgdnvk <bnovykov@gmail.com>
2023-05-08 23:54:09 +0200

    1

--- leetcode/1. Two Sum.ts
+++ leetcode/1. Two Sum.ts
@@ -1,12 +1,28 @@
 //https://leetcode.com/problems/two-sum/description/
-function twoSum(nums: number[], target: number): number[] {
-    const res: number[] = []
+// function twoSum(nums: number[], target: number): number[] {
+//     const res: number[] = []
 
+//     for(let i = 0; i < nums.length; i++) {
+//         for(let j = i+1; j < nums.length; j++) {
+//             if(nums[i] + nums[j] === target) res.push(i, j)
+//         }
+//     }
+
+//     return res
+// };
+// 
+
+function twoSum(nums: number[], target: number): number[] {
+    const map = new Map()
+    
     for(let i = 0; i < nums.length; i++) {
-        for(let j = i+1; j < nums.length; j++) {
-            if(nums[i] + nums[j] === target) res.push(i, j)
+        const diff = target - nums[i]
+        if(map.has(diff)) {
+            return [i, map.get(diff)]
+        } else {
+            map.set(nums[i], i)
         }
     }
-
-    return res
+    
+    return []
 };
