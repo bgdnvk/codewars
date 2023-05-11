b9b28028123acc26a800cddd0d25289c0eff0667
bgdnvk <bnovykov@gmail.com>
2023-05-11 03:33:11 +0200

    js day challenge

---
+++ leetcode/2629. Function Composition.ts
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/function-composition/description/
+type F = (x: number) => number;
+
+function compose(functions: F[]): F {
+	return function(x) {
+        for(let f of functions.reverse()) {
+            x = f(x)
+        }
+        return x 
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
