ecf33ce32eb1d9a3be4883090a5ad09e9b2ee561
bgdnvk <bnovykov@gmail.com>
2023-07-09 01:41:53 +0200

    238

--- leetcode/238. Product of Array Except Self.go
+++ leetcode/238. Product of Array Except Self.go
@@ -1,1 +1,20 @@
 //https://leetcode.com/problems/product-of-array-except-self/
+func productExceptSelf(nums []int) []int {
+    res := []int{}
+
+    start := 1
+    for _, v := range nums {
+        res = append(res, start)
+        start = v * start
+        fmt.Println("d", start)
+    }
+
+    //end
+    start = 1
+    for i := len(nums)-1; i >= 0; i-- {
+        res[i] = start * res[i]
+        start= start*nums[i]
+    }
+
+    return res
+}
