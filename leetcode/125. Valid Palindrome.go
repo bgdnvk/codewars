2649f007e780fb4afe1de8f1be0c54fee8cdc9cc
bgdnvk <bnovykov@gmail.com>
2023-07-11 03:28:58 +0200

    for loops go brr

--- leetcode/125. Valid Palindrome.go
+++ leetcode/125. Valid Palindrome.go
@@ -1,1 +1,20 @@
 //https://leetcode.com/problems/valid-palindrome/description/
+func isPalindrome(s string) bool {
+    arr := []rune{}
+    for _, v := range s {
+        if unicode.IsLetter(v) || unicode.IsDigit(v) {
+            letter := unicode.ToLower(v)
+            arr = append(arr, letter)
+        }
+    }
+
+    for i, _ := range arr {
+        first := arr[i] 
+        second := arr[len(arr) -i -1]
+        if first != second {
+            return false
+        }
+    }
+
+    return true
+}
