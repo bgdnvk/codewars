e1ceecfafc8bfe6eb824c6b216b49dbe64b1ab59
Bogdan Novykov Chumak <bnovykov@gmail.com>
2023-12-18 22:15:18 +0100

    217 go

--- leetcode/217. Contains Duplicate.go
+++ leetcode/217. Contains Duplicate.go
@@ -1,13 +1,38 @@
 func containsDuplicate(nums []int) bool {
-    set := make(map[int]struct{})
-    
-    for _, v := range nums {
-        if _, ok := set[v]; ok{
-            return true
-        } else {
-            set[v] = struct{}{}
-        }
-    }
+	set := make(map[int]struct{})
 
-    return false
+	for _, v := range nums {
+		if _, ok := set[v]; ok {
+			return true
+		} else {
+			set[v] = struct{}{}
+		}
+	}
+
+	return false
+}
+
+func containsduplicate(nums []int) bool {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] == nums[j] {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func containsDuplicate(nums []int) bool {
+	set := make(map[int]bool)
+
+	for _, v := range nums {
+		if _, ok := set[v]; ok {
+			return true
+		}
+		set[v] = true
+	}
+
+	return false
 }
