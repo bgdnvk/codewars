236439cb3613867e8f03624672191c33559cfa09
Bogdan Novykov Chumak <bnovykov@gmail.com>
2023-12-19 22:36:00 +0100

    anagra,

--- leetcode/242. Valid Anagram.go
+++ leetcode/242. Valid Anagram.go
@@ -1,24 +1,43 @@
 func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
 
-    if len(s) != len(t) {
-        return false
-    }
+	freq := make(map[rune]int)
 
-    freq := make(map[rune]int)
+	for _, v := range s {
+		freq[v]++
+	}
 
-    for _, v := range s{
-        freq[v]++
-    }
+	for _, v := range t {
 
-    for _, v := range t{
+		if freq[v] == 0 {
+			return false
+		}
 
-        if freq[v] == 0 {
-            return false
-        }
+		freq[v]--
+	}
 
-        freq[v]--
-    }
+	return true
+}
 
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
 
-    return true
+	freq := make(map[string]int)
+
+	for i := 0; i < len(s); i++ {
+		freq[string(s[i])]++
+	}
+
+	for i := 0; i < len(t); i++ {
+		if freq[string(t[i])] == 0 {
+			return false
+		}
+		freq[string(t[i])]--
+	}
+
+	return true
 }
