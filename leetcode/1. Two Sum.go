03d1cc008a29856d62a9018a81b2c35c1f319b87
Bogdan Novykov Chumak <bnovykov@gmail.com>
2023-12-21 20:48:06 +0100

    two sum

--- leetcode/1. Two Sum.go
+++ leetcode/1. Two Sum.go
@@ -1,14 +1,27 @@
 func twoSum(nums []int, target int) []int {
-    
-    m := make(map[int]int)
+	m := make(map[int]int)
 
-    for i, v := range nums {
+	for i, v := range nums {
 
-        if pos, ok := m[target-v]; ok {
-            return []int{pos, i}
-        }
-        m[v] = i
-    }
+		if pos, ok := m[target-v]; ok {
+			return []int{pos, i}
+		}
+		m[v] = i
+	}
 
-    return []int{}
- }
+	return []int{}
+}
+
+func twoSum(nums []int, target int) []int {
+	var slice []int
+
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if target-nums[i]-nums[j] == 0 {
+				slice = append(slice, i, j)
+			}
+		}
+	}
+
+	return slice
+}
