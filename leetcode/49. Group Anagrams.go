5f8216e82c5b2a7e42a0092695aff9b9a618e140
Bogdan Novykov Chumak <bnovykov@gmail.com>
2023-12-22 20:10:50 +0100

    anagrams

--- leetcode/49. Group Anagrams.go
+++ leetcode/49. Group Anagrams.go
@@ -1,19 +1,40 @@
+import "fmt"
+
 func groupAnagrams(strs []string) [][]string {
-    mp := map[[26]int][]string{}
+	mp := map[[26]int][]string{}
 
-    for idx, s := range strs {
-        fmt.Printf("i is %d and s is %s \n", idx, s)
-        k := [26]int{}
-        for i := 0; i < len(s); i++ {
-            k[s[i]-'a'] += 1
-        }
-        mp[k] = append(mp[k], s)
-    }
+	for idx, s := range strs {
+		fmt.Printf("i is %d and s is %s \n", idx, s)
+		k := [26]int{}
+		for i := 0; i < len(s); i++ {
+			k[s[i]-'a'] += 1
+		}
+		mp[k] = append(mp[k], s)
+	}
 
-    res := [][]string{}
-    for _, v := range mp {
-        res = append(res, v)
-    }
+	res := [][]string{}
+	for _, v := range mp {
+		res = append(res, v)
+	}
 
-    return res
+	return res
+}
+
+func groupAnagrams(strs []string) [][]string {
+	m := map[[26]int][]string{}
+
+	for _, v := range strs {
+		helper := [26]int{}
+
+		for _, s := range v {
+			helper[s-'a']++
+		}
+		m[helper] = append(m[helper], v)
+	}
+	res := [][]string{}
+	for _, words := range m {
+		res = append(res, words)
+	}
+
+	return res
 }
