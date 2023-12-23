eba8777006cd064dbb6f8e450f1974a23a77efd5
Bogdan Novykov Chumak <bnovykov@gmail.com>
2023-12-24 00:52:15 +0100

    top k freq

--- leetcode/347. Top K Frequent Elements.go
+++ leetcode/347. Top K Frequent Elements.go
@@ -1,25 +1,47 @@
-//https://leetcode.com/problems/top-k-frequent-elements/description/
+// https://leetcode.com/problems/top-k-frequent-elements/description/
 func topKFrequent(nums []int, k int) []int {
-    mp := map[int]int{}
-    
-    for _, v := range nums {
-        mp[v]++
-    }
+	mp := map[int]int{}
 
-    revmp := map[int][]int{}
-    for k, v := range mp{
-        revmp[v] = append(revmp[v], k)
-    }
+	for _, v := range nums {
+		mp[v]++
+	}
 
+	revmp := map[int][]int{}
+	for k, v := range mp {
+		revmp[v] = append(revmp[v], k)
+	}
 
-    res := []int{}
-    for i := len(nums); len(res) != k; i-- {
-        for _, v := range revmp[i] {
-            if len(res) != k {
-                res = append(res, v)
-            }
-        }
-    }
+	res := []int{}
+	for i := len(nums); len(res) != k; i-- {
+		for _, v := range revmp[i] {
+			if len(res) != k {
+				res = append(res, v)
+			}
+		}
+	}
 
-    return res
+	return res
+}
+
+func topKFrequent(nums []int, k int) []int {
+	m := make(map[int]int)
+	res := make([][]int, len(nums)+1)
+	result := []int{}
+
+	for _, v := range nums {
+		m[v]++
+	}
+
+	for k, v := range m {
+		res[v] = append(res[v], k)
+	}
+
+	for i := len(res) - 1; i > 0; i-- {
+		result = append(result, res[i]...)
+		if len(result) == k {
+			return result
+		}
+	}
+
+	return result
 }
