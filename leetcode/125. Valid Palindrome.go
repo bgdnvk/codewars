4a36cd6c9a8b96ccbab25e7c3e23a7e8016b1312
Bogdan Novykov Chumak <bnovykov@gmail.com>
2024-01-01 00:41:51 +0100

    pali

--- leetcode/125. Valid Palindrome.go
+++ leetcode/125. Valid Palindrome.go
@@ -1,20 +1,57 @@
-//https://leetcode.com/problems/valid-palindrome/description/
+import "unicode"
+
+// https://leetcode.com/problems/valid-palindrome/description/
 func isPalindrome(s string) bool {
-    arr := []rune{}
-    for _, v := range s {
-        if unicode.IsLetter(v) || unicode.IsDigit(v) {
-            letter := unicode.ToLower(v)
-            arr = append(arr, letter)
-        }
-    }
+	arr := []rune{}
+	for _, v := range s {
+		if unicode.IsLetter(v) || unicode.IsDigit(v) {
+			letter := unicode.ToLower(v)
+			arr = append(arr, letter)
+		}
+	}
 
-    for i, _ := range arr {
-        first := arr[i] 
-        second := arr[len(arr) -i -1]
-        if first != second {
-            return false
-        }
-    }
+	for i := range arr {
+		first := arr[i]
+		second := arr[len(arr)-i-1]
+		if first != second {
+			return false
+		}
+	}
 
-    return true
+	return true
+}
+
+func isPalindrome(s string) bool {
+	i := 0
+	j := len(s) - 1
+
+	arr := []rune(s)
+
+	for i < j {
+		l := unicode.ToLower(arr[i])
+		r := unicode.ToLower(arr[j])
+
+		if !isLetterOrDigit(l) {
+			i++
+			continue
+		}
+
+		if !isLetterOrDigit(r) {
+			j--
+			continue
+		}
+
+		if l != r {
+			return false
+		}
+
+		i++
+		j--
+	}
+
+	return true
+}
+
+func isLetterOrDigit(r rune) bool {
+	return unicode.IsLetter(r) || unicode.IsDigit(r)
 }
