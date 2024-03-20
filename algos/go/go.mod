675597d31badd754355c2d803509bc838082bf3a
Bogdan Novykov Chumak <bnovykov@gmail.com>
2024-03-20 22:51:13 +0100

    go bs

---
+++ algos/go/go.mod
@@ -0,0 +1,3 @@
+module algos
+
+go 1.22.1
---
+++ algos/go/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{42, -5, -3, -50, 1, 2, 3, 8, 6, 7, 5, 90, 20, 31}
+	fmt.Println("arr: ", arr)
+	// algo
+
+	// res
+	fmt.Println("sorted arr: ", arr)
+}
